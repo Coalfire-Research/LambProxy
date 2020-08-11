457a3acb1378257162d6a27135007b202463886c
Rick Osgood <rick@richardosgood.com>
2020-08-11 14:39:03 -0700

    Functional mitmproxy plugin

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # LambProxy
 HTTP Proxy using Amazon Lambda for source IP cycling
+
+## Overview
+This tool is a mitmproxy plugin that will intercept your HTTP/S requests and redirect them to an Amazon Lambda function. This way your request can be proxied through many IP addresses.
+
+## Usage
+    mitmproxy -p 8000 -s lambproxy.py --set roleArn='arn:aws:iam::123456789012:role/service-role/lambproxy-role-abcdefgh' --set maxWorkers=25
+    
+## Cleanup
+Right now Lambproxy has a hard time cleaning up the lambda workers automatically when mitmproxy exits. You can clean them manually within the mitmproxy console like:
+
+    :Lambproxy.cleanup
--- worker/lambproxy.py
+++ worker/lambproxy.py
@@ -2,23 +2,38 @@
 import json
 import socket
 import base64
+import ssl
 
-def forward_request(host, port, data):
+def forward_http_request(host, port, data):
    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-   socket.setdefaulttimeout(5)
+   socket.setdefaulttimeout(10)
    s.connect((host,port))
    s.send(data)
-   response = client.recv(4096)
-   return base64.b64encode(response)
+   response = s.recv(4096)
+   return base64.b64encode(response).decode('ascii')
+   
+def forward_https_request(host, port, data):
+   context = ssl.create_default_context()
+   s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+   ssock = context.wrap_socket(s, server_hostname=host)
+   socket.setdefaulttimeout(10)
+   ssock.connect((host,port))
+   ssock.send(data)
+   response = ssock.recv(4096)
+   return base64.b64encode(response).decode('UTF-8')
 
 def lambda_handler(event, context):
     host = event['host']
-    port = event['port']
+    port = int(event['port'])
     data = base64.b64decode(event['data'])
     
-    result = forward_request(host, proto, port)
+    if port == 443:
+        result = forward_https_request(host, port, data)
+    else:
+        result = forward_http_request(host, port, data)
     
     return {
         'statusCode': 200,
         'data': result
     }
+    
